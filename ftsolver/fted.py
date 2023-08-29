b4af72323ae6ebb88d73c75e170a881bf0466160
Chong Sun <sunchong137@gmail.com>
2023-08-28 20:16:17 -0500

    license

--- ftsolver/fted.py
+++ ftsolver/fted.py
@@ -1,8 +1,20 @@
+#   Copyright 2016-2023 Chong Sun
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
 '''
 exact diagonalization solver with grand canonical statistics.
-Chong Sun 08/07/17
-taking temperature (T) and chemical potential (mu) as input
-Chong Sun 01/16/18
 '''
 
 import numpy as np
